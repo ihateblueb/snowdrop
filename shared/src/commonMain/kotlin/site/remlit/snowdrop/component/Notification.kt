ihateblueb/snowdrop

commit 1c157278d87db9864157afd4d81eccd7e44042bb

don't show sharkey reaction notifs since they don't even provide the actual reaction

shared/src/commonMain/kotlin/site/remlit/snowdrop/component/Notification.kt
@@ -55,6 +55,10 @@ import snowdrop.shared.generated.resources.icon_tooth_24px
 fun Notification(notification: Notification) {
 	val navHandler = LocalNavController.current
 
+	// sharkey doesn't include the actual reactions in the notifications for some reason
+	// chuckya includes the reaction prop, so we should use that. otherwise there's no point in showing the notif
+	if (notification.type == "reaction" && notification.reaction == null) return
+
 	if (notification.type == "mention" && notification.status != null) {
 		Status(notification.status)
 	} else {
@@ -72,6 +76,10 @@ fun Notification(notification: Notification) {
 							tint = MaterialTheme.colorScheme.primary
 						)
 						"pleroma:emoji_reaction" -> Icon(
+							painterResource(Res.drawable.icon_add_24px), null,
+							tint = MaterialTheme.colorScheme.primary
+						)
+						"reaction" -> Icon(
 							painterResource(Res.drawable.icon_add_24px), null,
 							tint = MaterialTheme.colorScheme.primary
 						)
@@ -114,7 +122,7 @@ fun Notification(notification: Notification) {
 						var message by remember { mutableStateOf("") }
 
 						when (notification.type) {
-							"favourite", "pleroma:emoji_reaction", "reblog", "update", "status", "bite" ->
+							"favourite", "pleroma:emoji_reaction", "reaction", "reblog", "update", "status", "bite" ->
 								displayName = notification.account.displayName
 									?: notification.account.username
 						}
@@ -122,6 +130,7 @@ fun Notification(notification: Notification) {
 						when (notification.type) {
 							"favourite" -> message = "liked your post"
 							"pleroma:emoji_reaction" -> message = "reacted with ${notification.emoji}"
+							"reaction" -> message = "reacted with :${notification.reaction?.name}:"
 							"reblog" -> message = "boosted your post"
 							"update" -> message = "edited a post"
 							"poll" -> message = "A poll you have voted in has ended"

shared/src/commonMain/kotlin/site/remlit/snowdrop/model/Notification.kt
@@ -14,6 +14,7 @@ data class Notification(
 	val status: Status? = null,
 
 	val emoji: String? = null,
+	val reaction: ChuckyaReaction? = null,
 	@SerialName("emoji_url")
 	val emojiUrl: String? = null,
 	val bite: Bite? = null,
@@ -21,6 +22,14 @@ data class Notification(
 	@SerialName("created_at")
 	val createdAt: String
 ) {
+	@Serializable
+	data class ChuckyaReaction(
+		val name: String? = null,
+		val id: String? = null,
+		val url: String? = null,
+		@SerialName("static_url")
+		val staticUrl: String? = null
+	)
 	@Serializable
 	data class Bite(
 		val id: String,

commit f418ebdce2e34381922c994d66af4c296a246479

fix non-pleroma reactions regression from 1c157278d87db9864157afd4d81eccd7e44042bb

shared/src/commonMain/kotlin/site/remlit/snowdrop/component/Notification.kt
@@ -29,7 +29,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withLink
 import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import org.jetbrains.compose.resources.painterResource
@@ -72,7 +71,7 @@ fun Notification(notification: Notification) {
 
 	// sharkey doesn't include the actual reactions in the notifications for some reason
 	// chuckya includes the reaction prop, so we should use that. otherwise there's no point in showing the notif
-	if (notification.type == "reaction" && notification.reaction == null) return
+	if (notification.type == "reaction" && notification.reaction == null && notification.emoji == null) return
 
 	if (notification.type == "mention" && notification.status != null) {
 		Status(notification.status)
@@ -154,7 +153,9 @@ fun Notification(notification: Notification) {
 						when (notification.type) {
 							"favourite" -> message = stringResource(Res.string.liked_your_post)
 							"pleroma:emoji_reaction" -> message = stringResource(Res.string.reacted_with_x, "${notification.emoji}")
-							"reaction" -> message = stringResource(Res.string.reacted_with_x, ":${notification.reaction?.name}:")
+							"reaction" -> message =
+								if (notification.reaction == null) stringResource(Res.string.reacted_with_x, "${notification.emoji}")
+								else stringResource(Res.string.reacted_with_x, ":${notification.reaction.name}:")
 							"reblog" -> message = stringResource(Res.string.boosted_your_post)
 							"update" -> message = stringResource(Res.string.edited_a_post)
 							"poll" -> message = stringResource(Res.string.a_poll_you_have_voted_in_has_ended)