2fcb832fc7579908fcac53df5fe9e0fedd077a97
notfire <notfire@posteo.net>
2026-07-02 13:42:11 -0400

    don't include dependency metadata in built apks f-droid doesn't like these because they're signed with google's keys

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -45,4 +45,9 @@ android {
 		sourceCompatibility = JavaVersion.VERSION_11
 		targetCompatibility = JavaVersion.VERSION_11
 	}
+	// so f-droid is happy
+	dependenciesInfo {
+		includeInApk = false
+		includeInBundle = false
+	}
 }
