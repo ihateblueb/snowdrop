ec63a8f14bfdd7624e26a16f072d5148f4d337b5
ihateblueb <ihateblueb@proton.me>
2026-07-31 16:58:40 -0400

    Fix awful reacted by page

--- shared/src/commonMain/kotlin/site/remlit/snowdrop/view/StatusInteractionDetailView.kt
+++ shared/src/commonMain/kotlin/site/remlit/snowdrop/view/StatusInteractionDetailView.kt
@@ -1,6 +1,9 @@
 package site.remlit.snowdrop.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.PrimaryScrollableTabRow
@@ -8,11 +11,17 @@ import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import site.remlit.snowdrop.api.statuses.getStatusFavouritedBy
@@ -25,6 +34,7 @@ import site.remlit.snowdrop.model.Account
 import site.remlit.snowdrop.model.ApiResponse
 import site.remlit.snowdrop.model.Emoji
 import site.remlit.snowdrop.model.Reaction
+import site.remlit.snowdrop.model.Status
 import site.remlit.snowdrop.util.LocalNavController
 import site.remlit.snowdrop.util.LocalSnackbarController
 import site.remlit.snowdrop.util.bgIO
@@ -33,6 +43,7 @@ import snowdrop.shared.generated.resources.boosted_by
 import snowdrop.shared.generated.resources.icon_arrow_back_24
 import snowdrop.shared.generated.resources.liked_by
 import snowdrop.shared.generated.resources.reacted_by
+import kotlin.collections.forEach
 
 enum class InteractionViewType {
 	Boost, Like, Reaction
@@ -45,6 +56,7 @@ fun StatusInteractionDetailView(
 ) = ViewSurface {
 	val navHandler = LocalNavController.current
 	val snackbarHandler = LocalSnackbarController.current
+	val coroutineScope = rememberCoroutineScope()
 
 	TopAppBar(
 		navigationIcon = {
@@ -84,36 +96,71 @@ fun StatusInteractionDetailView(
 			}
 		}
 		InteractionViewType.Reaction -> {
-			var currentTab by remember { mutableStateOf(0) }
+			var currentTab by remember { mutableStateOf<String?>(null) }
 			val reactions = remember { mutableStateListOf<Reaction>() }
-			bgIO {
-				val res = getStatusReactions(id)
-				if (res.error || res.response == null) {
-					res.handleError(snackbarController = snackbarHandler)
-					return@bgIO
+			val tabs = remember { mutableStateListOf<String>() }
+
+			LaunchedEffect(Unit) {
+				coroutineScope.launch {
+					reactions.clear()
+					val res = getStatusReactions(id)
+					if (res.error || res.response == null) {
+						res.handleError(snackbarController = snackbarHandler)
+						return@launch
+					}
+					reactions.addAll(res.response)
+
+					if (reactions.isEmpty()) return@launch
+
+					reactions.forEach {
+						if (!tabs.contains(it.name)) tabs.add(it.name)
+					}
+
+					currentTab = tabs.firstOrNull()
 				}
-				reactions.addAll(res.response)
 			}
 
-			// todo: scrolling
+
 			if (!reactions.isEmpty()) {
-				PrimaryScrollableTabRow(currentTab) {
-					reactions.forEach {
-						val i = reactions.indexOf(it)
-						Tab(
-							selected = currentTab == i,
-							onClick = { currentTab = i },
-							text = {
-								if (it.url != null) {
-									Emoji(Emoji(it.name, it.staticUrl ?: it.url, it.url))
-								} else Text(it.name)
-							}
-						)
+				key(currentTab) {
+					PrimaryScrollableTabRow(
+						selectedTabIndex = if (currentTab != null) tabs.indexOf(currentTab) else 0,
+						edgePadding = 15.dp
+					) {
+						tabs.forEach { tab ->
+							val reaction = reactions.first { it.name == tab }
+							val count = reactions.count { it.name == tab }
+
+							Tab(
+								selected = currentTab == tab,
+								onClick = { currentTab = tab },
+								text = {
+									Row(
+										horizontalArrangement = Arrangement.spacedBy(5.dp)
+									) {
+										site.remlit.snowdrop.component.Reaction(
+											Status.Reaction(
+												count = count.toLong(),
+												me = false,
+												name = reaction.name,
+												url = reaction.url,
+												staticUrl = reaction.staticUrl
+											)
+										)
+
+										Text("$count")
+									}
+								}
+							)
+						}
 					}
-				}
-				LazyColumn {
-					reactions.filter { reactions.indexOf(it) == currentTab }.forEach { reaction ->
-						item { AccountRow(account = reaction.account) }
+					LazyColumn {
+						items(
+							items = reactions.filter { it.name == currentTab },
+							key = { it.account.id }
+						) {
+							AccountRow(account = it.account)
+						}
 					}
 				}
 			}
