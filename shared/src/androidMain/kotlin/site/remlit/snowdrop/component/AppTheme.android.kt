289e44aa910289411a5e0763d72ea83ff6f3a537
notfire <notfire@posteo.net>
2026-08-12 15:28:05 -0400

    fix crashes from android 7 to 11

--- shared/src/androidMain/kotlin/site/remlit/snowdrop/component/AppTheme.android.kt
+++ shared/src/androidMain/kotlin/site/remlit/snowdrop/component/AppTheme.android.kt
@@ -3,15 +3,27 @@ package site.remlit.snowdrop.component
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 @Composable
-@RequiresApi(Build.VERSION_CODES.S)
-actual fun getLightColorScheme(): ColorScheme = dynamicLightColorScheme(LocalContext.current)
+actual fun getLightColorScheme(): ColorScheme {
+	return if (Build.VERSION.SDK_INT < 31) {
+		lightColorScheme()
+	} else {
+		dynamicLightColorScheme(LocalContext.current)
+	}
+}
 
 @Composable
-@RequiresApi(Build.VERSION_CODES.S)
-actual fun getDarkColorScheme(): ColorScheme = dynamicDarkColorScheme(LocalContext.current)
+actual fun getDarkColorScheme(): ColorScheme {
+	return if (Build.VERSION.SDK_INT < 31) {
+		darkColorScheme()
+	} else {
+		dynamicDarkColorScheme(LocalContext.current)
+	}
+}
