65b5010260a0ebf975a0dbafd5f9f1b2a034d323
nicole mikołajczyk <git@mkljczk.pl>
2026-08-16 17:30:04 +0200

    Provide auth token to custom emojis request
    Signed-off-by: nicole mikołajczyk <git@mkljczk.pl>

--- shared/src/commonMain/kotlin/site/remlit/snowdrop/api/getEmojis.kt
+++ shared/src/commonMain/kotlin/site/remlit/snowdrop/api/getEmojis.kt
@@ -1,14 +1,21 @@
 package site.remlit.snowdrop.api
 
+import com.russhwolf.settings.ExperimentalSettingsApi
 import io.ktor.client.request.get
+import io.ktor.client.request.header
 import site.remlit.snowdrop.model.ApiResponse
 import site.remlit.snowdrop.model.Emoji
 import site.remlit.snowdrop.util.config.endOfRequest
 import site.remlit.snowdrop.util.config.httpClient
 import site.remlit.snowdrop.util.safeApiRequest
+import site.remlit.snowdrop.util.settings
 
-suspend fun getEmojis(): ApiResponse<List<Emoji>> = safeApiRequest { _, host ->
-	val req = httpClient.get("https://$host/api/v1/custom_emojis")
+@OptIn(ExperimentalSettingsApi::class)
+suspend fun getEmojis(): ApiResponse<List<Emoji>> = safeApiRequest { accountId, host ->
+	val token = settings.getString("account_${accountId}_token", "")
+	val req = httpClient.get("https://$host/api/v1/custom_emojis") {
+		header("Authorization", "Bearer $token")
+	}
 
 	endOfRequest(req)
 }
