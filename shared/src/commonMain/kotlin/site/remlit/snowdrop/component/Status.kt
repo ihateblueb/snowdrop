75e69caec738b43c8c3c5a49c7e42822bd098758
ihateblueb <ihateblueb@proton.me>
2026-09-09 15:52:05 -0400

    Fix imports

--- shared/src/commonMain/kotlin/site/remlit/snowdrop/component/Status.kt
+++ shared/src/commonMain/kotlin/site/remlit/snowdrop/component/Status.kt
@@ -23,8 +23,6 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.PlainTooltip
 import androidx.compose.material3.Text
