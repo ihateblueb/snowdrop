d63c60d3f8c64840b5439a5041efe5e77d434992
notfire <notfire@posteo.net>
2026-09-14 10:16:13 -0400

    [api] add routes to mute/unmute conversations

---
+++ shared/src/commonMain/kotlin/site/remlit/snowdrop/api/statuses/unmuteStatus.kt
@@ -0,0 +1,22 @@
+package site.remlit.snowdrop.api.statuses
+
+import com.russhwolf.settings.ExperimentalSettingsApi
+import io.ktor.client.request.header
+import io.ktor.client.request.post
+import site.remlit.snowdrop.model.ApiResponse
+import site.remlit.snowdrop.model.Status
+import site.remlit.snowdrop.util.config.endOfRequest
+import site.remlit.snowdrop.util.config.httpClient
+import site.remlit.snowdrop.util.safeApiRequest
+import site.remlit.snowdrop.util.settings
+
+@OptIn(ExperimentalSettingsApi::class)
+suspend fun unmuteStatus(id: String): ApiResponse<Status> = safeApiRequest { accountId, host ->
+	val token = settings.getString("account_${accountId}_token", "")
+
+	val req = httpClient.post("https://$host/api/v1/statuses/$id/unmute") {
+		header("Authorization", "Bearer $token")
+	}
+
+	endOfRequest(req)
+}
